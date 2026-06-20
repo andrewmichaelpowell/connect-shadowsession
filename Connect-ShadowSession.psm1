21df1036accdeb265307f189d4d1abfdd71eda5c
Andrew Powell <andrewmichaelpowell@icloud.com>
2026-06-20 23:29:23 +0200

    Format code

--- Connect-ShadowSession.psm1
+++ Connect-ShadowSession.psm1
@@ -1,24 +1,19 @@
 # Connect-ShadowSession
 # github.com/andrewmichaelpowell
 
-Function Connect-ShadowSession
-{
+Function Connect-ShadowSession {
   Param(
     [Parameter(Mandatory="True")]
     [String]$Computer
   )
 
-    If(Test-Connection -ComputerName $Computer -Count 1 -Quiet)
-    {
+  If(Test-Connection -ComputerName $Computer -Count 1 -Quiet) {
       query.exe session /server:$Computer
       $Session = Read-Host -Prompt "Enter Session ID"
       mstsc.exe /V:$Computer /shadow:$Session /control /noconsentprompt
   }
-
-  Else
-  {
-    Try
-    {
+  Else {
+    Try {
       $Resolve = Resolve-DNSName -ErrorAction Stop -Name $Computer
       If(($Resolve.IPAddress).IndexOf(".") -gt 0){
         Write-Host ""
@@ -26,18 +21,14 @@ Function Connect-ShadowSession
         Write-Host -NoNewLine -ForegroundColor White $Computer.ToLower()
         Write-Host -ForegroundColor Yellow " has a DNS record, but it is currently offline."
       }
-
-      Else
-      {
+      Else {
         Write-Host ""
         Write-Host -NoNewLine -ForegroundColor Yellow "Host "
         Write-Host -NoNewLine -ForegroundColor White $Computer.ToLower()
         Write-Host -ForegroundColor Yellow " does not have a DNS record."
       }
     }
-
-    Catch
-    {
+  Catch {
       Write-Host ""
       Write-Host -NoNewLine -ForegroundColor Yellow "Host "
       Write-Host -NoNewLine -ForegroundColor White $Computer.ToLower()
